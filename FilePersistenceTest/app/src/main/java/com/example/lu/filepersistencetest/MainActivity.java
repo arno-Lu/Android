2a951238e870a4b2f767176e771ca9449908a54e
lucky-code <765070327@qq.com>
2016-03-24 21:27:10 +0800

    FilePersistenceTest

---
+++ FilePersistenceTest/app/src/main/java/com/example/lu/filepersistencetest/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.lu.filepersistencetest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends Activity {
+
+    private EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit = (EditText) findViewById(R.id.edit);
+        String inputText =load();
+        if(!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);                              //设置文本
+            edit.setSelection(inputText.length());                //移动光标到结尾
+            Toast.makeText(this,"Restoring successed",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        String inputText = edit.getText().toString();
+        save(inputText);
+    }
+
+    public void save(String inputText){
+        FileOutputStream out =null;
+        BufferedWriter writer = null ;
+        try {
+            out = openFileOutput("date", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if(writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String load(){
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuffer content = new StringBuffer();
+        try{
+            in = openFileInput("date");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = " ";
+            while((line = reader.readLine())!=null){
+                content.append(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(reader!=null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
+    }
+}
