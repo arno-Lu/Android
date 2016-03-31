4869cb3b5201e916d6d8771d5b578cd658df23bf
lucky-code <765070327@qq.com>
2016-03-31 20:53:35 +0800

    TitleTest

---
+++ TitleTest/app/src/main/java/com/example/lu/titletest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lu.titletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
