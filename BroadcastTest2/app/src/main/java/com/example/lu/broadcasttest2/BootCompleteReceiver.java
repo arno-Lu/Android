743f60d91a494020fcdc215c5e13b0f6b58e1f52
lucky-code <765070327@qq.com>
2016-03-31 20:11:44 +0800

    BroadcastTest2
    静态广播

---
+++ BroadcastTest2/app/src/main/java/com/example/lu/broadcasttest2/BootCompleteReceiver.java
@@ -0,0 +1,18 @@
+package com.example.lu.broadcasttest2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context,"Boot Complete" ,Toast.LENGTH_LONG).show();
+
+    }
+
+}
