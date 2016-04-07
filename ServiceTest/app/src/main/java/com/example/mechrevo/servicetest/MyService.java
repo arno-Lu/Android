63b5f28997b5b28e31773d72c96f1f471e5af274
lucky-code <765070327@qq.com>
2016-04-07 21:23:10 +0800

    ServiceTest

---
+++ ServiceTest/app/src/main/java/com/example/mechrevo/servicetest/MyService.java
@@ -0,0 +1,54 @@
+package com.example.mechrevo.servicetest;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder{
+
+        public void startDownload(){
+            Log.d("MyService","startDownload executed");
+        }
+        public int getProgress(){
+            Log.d("MyService","getProgress executed" );
+            return 0;
+        }
+    }
+
+    public MyService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        Log.d("MyService","onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId){
+
+        Log.d("MyService","onStartCommand executed");
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("MyService","onDestroy executed");
+    }
+}
