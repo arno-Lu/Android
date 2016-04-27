771c7ce1a102d4a615bca101a3471ee9d7b9fb29
lucky-code <765070327@qq.com>
2016-04-27 21:04:24 +0800

    Bluetooth find

--- BluetoothTest/app/src/main/java/com/example/mechrevo/bluetoothtest/MainActivity.java
+++ BluetoothTest/app/src/main/java/com/example/mechrevo/bluetoothtest/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mechrevo.bluetoothtest;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,43 +16,77 @@ import android.widget.Toast;
 import java.util.Iterator;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button button;
+    private Button buttonStart;
+    private Button buttonFind;
     private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button)findViewById(R.id.btn);
+        buttonStart = (Button)findViewById(R.id.btn);
+        buttonFind = (Button)findViewById(R.id.find);
         textView = (TextView)findViewById(R.id.text_view);
-        button.setOnClickListener(new ButtonListener());
+        buttonStart.setOnClickListener(this);
+        buttonFind.setOnClickListener(this);
     }
 
-    private class ButtonListener implements View.OnClickListener{
+        public void onClick(View v) {
+            switch (v.getId()){
+                case  R.id.btn:
+                    BluetoothAdapter  adapter = BluetoothAdapter.getDefaultAdapter();
+                    if(adapter!=null){
+                        Toast.makeText(MainActivity.this,"本机拥有蓝牙设备",Toast.LENGTH_SHORT).show();
+                        if(!adapter.isEnabled()){
+                            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            Toast.makeText(MainActivity.this,"开始启动蓝牙",Toast.LENGTH_SHORT).show();
+                            startActivity(intent);
 
-        public void onClick(View v){
-            BluetoothAdapter  adapter = BluetoothAdapter.getDefaultAdapter();
-            if(adapter!=null){
-                Toast.makeText(MainActivity.this,"本机拥有蓝牙设备",Toast.LENGTH_SHORT).show();
-                if(!adapter.isEnabled()){
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    Toast.makeText(MainActivity.this,"开始启动蓝牙",Toast.LENGTH_SHORT).show();
-                    startActivity(intent);
+                        }
+                        Set<BluetoothDevice> devices = adapter.getBondedDevices();
+                        if(devices.size()>0){
+                            for(Iterator iterator = devices.iterator();iterator.hasNext();){
+                                BluetoothDevice device = (BluetoothDevice) iterator.next();
+                                textView.setText("123");
+                                textView.setText(device.getAddress());
+                            };
+                        }
+                    }else
+                    {
+                        Toast.makeText(MainActivity.this,"本机没有蓝牙设备",Toast.LENGTH_SHORT).show();
+                    }
+                case R.id.find:
+                    Intent findIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    findIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,500);
+                    startActivity(findIntent);
 
-                }
-                Set<BluetoothDevice> devices = adapter.getBondedDevices();
-                if(devices.size()>0){
-                    for(Iterator iterator = devices.iterator();iterator.hasNext();){
-                        BluetoothDevice device = (BluetoothDevice) iterator.next();
-                        textView.setText("123");
-                        textView.setText(device.getAddress());
-                    };
-                }
-            }else
-            {
-                Toast.makeText(MainActivity.this,"本机没有蓝牙设备",Toast.LENGTH_SHORT).show();
+                    BluetoothAdapter bluetoothAdapter = null;
+                    BluetoothReceiver bluetoothReceiver = null;
+
+                    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                    bluetoothReceiver = new BluetoothReceiver();
+
+                    registerReceiver(bluetoothReceiver,intentFilter);
+
+                    bluetoothAdapter.startDiscovery();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    private class BluetoothReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                textView.setText(bluetoothDevice.getAddress());
             }
         }
     }
